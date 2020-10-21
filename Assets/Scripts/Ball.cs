904e519cba4bf25b716abeaa413b432ba8a4afee
kennethpino <ken.pino@gmail.com>
2020-10-21 09:27:56 +0200

    DIN-7 Added collision and ball/paddle dyamics

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float xDirection = 2f;
+    [SerializeField] private float yDirection = 15f;
+    [SerializeField] private Paddle paddle;
+    private Vector2 ballToPaddleOffset;
+    private bool isBallLocked = true;
+
+    void Start()
+    {
+        ballToPaddleOffset = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        if (isBallLocked)
+        {
+            LockBallOnPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xDirection, yDirection);
+            isBallLocked = false;
+        }
+    }
+
+    private void LockBallOnPaddle()
+    {
+        transform.position = new Vector2(paddle.transform.position.x + ballToPaddleOffset.x, paddle.transform.position.y + ballToPaddleOffset.y);
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,11 +3,15 @@
 public class Paddle : MonoBehaviour
 {
     [SerializeField] private float screenSizeInUnitsX = 16f;
+    [SerializeField] private float minClamp = 1f;
+    [SerializeField] private float maxClamp = 16f;
 
     void Update()
     {
-        float xPos = Input.mousePosition.x / Screen.width * screenSizeInUnitsX;
-        Vector2 paddlePos = new Vector2(xPos, transform.position.y);
+        float mouseXPos = Input.mousePosition.x / Screen.width * screenSizeInUnitsX;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseXPos, minClamp, maxClamp);
         transform.position = paddlePos;
     }
 }
+    
